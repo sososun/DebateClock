74c2bf6cbea323bc0776089d4f98d8ce661110b2
sososun <350320367@qq.com>
2017-02-19 02:40:57 +0800

    基本编辑界面和初步的时钟界面提交，xml将在定型以后整体复用至style,demin和string也将被规范化设计

---
+++ app/src/main/java/com/sunxinyang/betaclock/GameRuleSettingActivity.java
@@ -0,0 +1,16 @@
+package com.sunxinyang.betaclock;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by soso-sun on 2017/2/18.
+ */
+
+public class GameRuleSettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_rule_setting);
+    }
+}
