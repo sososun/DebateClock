fa7ee1e2b613985e4680f1a1ffb1184ba850c83c
sososun <350320367@qq.com>
2017-02-20 01:27:34 +0800

    二次提交页面，基本页面逻辑完成

---
+++ app/src/main/java/com/sunxinyang/betaclock/GamingActivity.java
@@ -0,0 +1,18 @@
+package com.sunxinyang.betaclock;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by soso-sun on 2017/2/20.
+ */
+
+public class GamingActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_clock_activity);
+    }
+}
--- app/src/main/java/com/sunxinyang/betaclock/MainActivity.java
+++ app/src/main/java/com/sunxinyang/betaclock/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this,GameRuleSettingActivity.class));
-                finish();
+//                finish();
             }
         });
     }
